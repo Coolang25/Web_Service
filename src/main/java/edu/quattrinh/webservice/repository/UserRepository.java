e1958a86f2a5783fb714c83329f60fc915ceaad4
Quat Trinh <quattrinh.cs@gmail.com>
2024-07-11 16:24:23 +0700

    Initialize the project

---
+++ src/main/java/edu/quattrinh/webservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package edu.quattrinh.webservice.repository;
+
+import edu.quattrinh.webservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+
+}
---
+++ src/main/java/edu/quattrinh/webservice/service/UserService.java
@@ -0,0 +1,55 @@
+package edu.quattrinh.webservice.service;
+
+import edu.quattrinh.webservice.dto.request.UserCreationRequest;
+import edu.quattrinh.webservice.dto.request.UserUpdateRequest;
+import edu.quattrinh.webservice.entity.User;
+import edu.quattrinh.webservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String userId) {
+        return userRepository.findById(userId).orElseThrow(() ->
+                new RuntimeException("User not found"));
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(() ->
+                new RuntimeException("User not found"));
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+}
