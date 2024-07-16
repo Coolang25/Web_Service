df553d8fab44030ae4b589da675a68167276755f
Quat Trinh <quattrinh.cs@gmail.com>
2024-07-16 17:18:21 +0700

    Unit test for controller, servicec

---
+++ src/test/java/edu/quattrinh/webservice/controller/UserControllerTest.java
@@ -0,0 +1,102 @@
+package edu.quattrinh.webservice.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import edu.quattrinh.webservice.dto.request.UserCreationRequest;
+import edu.quattrinh.webservice.dto.response.UserResponse;
+import edu.quattrinh.webservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private UserCreationRequest request;
+    private UserResponse response;
+    private LocalDate dob;
+    @BeforeEach
+    private void initData() {
+
+        dob = LocalDate.of(1999, 11, 25);
+
+        request = UserCreationRequest.builder()
+                .username("quattrinh")
+                .firstName("Quat")
+                .lastName("Trinh")
+                .password("12345678")
+                .dob(dob)
+                .build();
+
+        response = UserResponse.builder()
+                .id("sagagagwegwegwe")
+                .username("quattrinh")
+                .firstName("Quat")
+                .lastName("Trinh")
+                .dob(dob)
+                .build();
+
+    }
+
+    @Test
+    void createUser_validRequest_success() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        Mockito.when(userService.createUser(ArgumentMatchers.any()))
+                .thenReturn(response);
+
+
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/users")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.id").value("sagagagwegwegwe"));
+
+    }
+
+    @Test
+    void createUser_usernameInvalid_fail() throws Exception {
+        // GIVEN
+        request.setUsername("jo");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1003))
+                .andExpect(MockMvcResultMatchers.jsonPath("message").value("Username must be at least 3 characters"));
+
+    }
+}
---
+++ src/test/java/edu/quattrinh/webservice/service/UserServiceTest.java
@@ -0,0 +1,92 @@
+package edu.quattrinh.webservice.service;
+
+import edu.quattrinh.webservice.dto.request.UserCreationRequest;
+import edu.quattrinh.webservice.dto.response.UserResponse;
+import edu.quattrinh.webservice.entity.User;
+import edu.quattrinh.webservice.exception.AppException;
+import edu.quattrinh.webservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+    private UserCreationRequest request;
+    private UserResponse response;
+    private LocalDate dob;
+    private User user;
+
+    @BeforeEach
+    private void initData() {
+
+        dob = LocalDate.of(1999, 11, 25);
+
+        request = UserCreationRequest.builder()
+                .username("quattrinh")
+                .firstName("Quat")
+                .lastName("Trinh")
+                .password("12345678")
+                .dob(dob)
+                .build();
+
+//        response = UserResponse.builder()
+//                .id("sagagagwegwegwe")
+//                .username("quattrinh")
+//                .firstName("Quat")
+//                .lastName("Trinh")
+//                .dob(dob)
+//                .build();
+
+        user = User.builder()
+                .id("sagagagwegwegwe")
+                .username("quattrinh")
+                .firstName("Quat")
+                .lastName("Trinh")
+                .dob(dob)
+                .build();
+
+    }
+
+    @Test
+    void createUser_validRequest_success() {
+        // GIVEN
+        Mockito.when(userRepository.existsByUsername(Mockito.anyString())).thenReturn(false);
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+
+        // WHEN
+        var response = userService.createUser(request);
+
+        // THEN
+        Assertions.assertThat(response.getId()).isEqualTo("sagagagwegwegwe");
+        Assertions.assertThat(response.getUsername()).isEqualTo("quattrinh");
+    }
+
+    @Test
+    void createUser_userExisted_fail() {
+        // GIVEN
+        Mockito.when(userRepository.existsByUsername(Mockito.anyString())).thenReturn(true);
+
+        // WHEN
+        var exception = org.junit.jupiter.api.Assertions.assertThrows(AppException.class, () -> userService.createUser(request));
+
+        // THEN
+        Assertions.assertThat(exception.getErrorCode().getCode())
+                .isEqualTo(1002);
+    }
+
+
+
+}
